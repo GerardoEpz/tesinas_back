cc8cb42c828ca6485ceee00de2724d214d778213
eliuabdiel <2018030333@upsin.edu.mx>
2021-03-21 00:07:14 -0700

    added update FAQ method and endpoint

--- src/main/java/com/tesinas/spring/jwt/mongodb/controllers/FaqController.java
+++ src/main/java/com/tesinas/spring/jwt/mongodb/controllers/FaqController.java
@@ -2,16 +2,10 @@ package com.tesinas.spring.jwt.mongodb.controllers;
 
 import com.tesinas.spring.jwt.mongodb.models.Faq;
 import com.tesinas.spring.jwt.mongodb.repository.FaqRepository;
-import com.tesinas.spring.jwt.mongodb.security.services.UserDetailsImpl;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,11 +13,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/faq")
 public class FaqController {
+
     @Autowired
     FaqRepository faqRepository;
+
     @GetMapping("/getall")
     public ResponseEntity<?> requestAllQuestions(){
         List<Faq> faqs = faqRepository.findAll();
         return ResponseEntity.ok(faqs);
+    }
+
+    @PostMapping("/addFaq")
+//    @PreAuthorize(" hasRole('PROFESOR') or hasRole('ASESOR') or hasRole('DIRECTOR')")
+    public ResponseEntity<?> addFaq(@RequestBody Faq faq){
+        try {
+            faqRepository.save(faq);
+            return ResponseEntity.ok().body("FAQ Updated");
+        } catch (Exception error){
+            return ResponseEntity.badRequest().body("Could not update FAQ");
+        }
     }
 }
